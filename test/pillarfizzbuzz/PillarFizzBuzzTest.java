54eab622a9d26b16f9253651eed4d6712101f7b0
joexi <joexi@PickAxeBlack.hsd1.mi.comcast.net>
2018-09-07 20:39:15 -0400

    Genesis+

---
+++ test/pillarfizzbuzz/PillarFizzBuzzTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pillarfizzbuzz;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joexi
+ */
+public class PillarFizzBuzzTest {
+    
+    public PillarFizzBuzzTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedANumberItReturnsThatNumber(){
+        PillarFizzBuzz fizzBuzz = new PillarFizzBuzz();
+        assertEquals("1", fizzBuzz.fizz(1));
+    }
+    
+}
