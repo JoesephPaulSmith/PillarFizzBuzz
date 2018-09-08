96576c2cc8952e355d29ac79ee61526c57c31bb9
joexi <joexi@PickAxeBlack.hsd1.mi.comcast.net>
2018-09-07 20:56:06 -0400

    Buzz Part 1 of n

--- src/pillarfizzbuzz/PillarFizzBuzz.java
+++ src/pillarfizzbuzz/PillarFizzBuzz.java
@@ -15,6 +15,9 @@ public class PillarFizzBuzz {
         if(inputNumber % 3 == 0){
             return("fizz");
         }
+        else if(inputNumber % 5 == 0){
+            return("buzz");
+        }
         return(inputNumber.toString());
     }
 
--- test/pillarfizzbuzz/PillarFizzBuzzTest.java
+++ test/pillarfizzbuzz/PillarFizzBuzzTest.java
@@ -56,4 +56,10 @@ public class PillarFizzBuzzTest {
         assertEquals("fizz", fizzBuzz.fizz(6));
     }
     
+    @Test
+    public void whenFizzBuzzIsPassedAMultipleOfFiveItReturnsBuzz(){
+        assertEquals("buzz", fizzBuzz.fizz(5));
+        assertEquals("buzz", fizzBuzz.fizz(10));
+    }
+    
 }
